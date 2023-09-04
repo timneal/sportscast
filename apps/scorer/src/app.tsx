9dc144f557116fcde987fab9928ca50397ae91a8
Tim Neal <tim.neal@dolby.com>
2023-09-05 09:03:26 +1000

    Adding state update to the viewer (more hackery!)

--- apps/scorer/src/app.tsx
+++ apps/scorer/src/app.tsx
@@ -1,4 +1,4 @@
-import { Button, Box, Input, HStack, Heading, Text, VStack, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
+import { Button, Box, Input, InputGroup, InputLeftAddon, HStack, Heading, Text, VStack, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 import PubNub from 'pubnub';
 import { PubNubProvider, usePubNub } from 'pubnub-react';
@@ -10,7 +10,6 @@ const pubnub = new PubNub({
   subscribeKey: 'sub-c-59bf0b71-9aa4-4168-9e57-139036b8bfd3',
   uuid: 'lijojatis'
 });
-
 
 function App() {
   return (
@@ -27,31 +26,66 @@ function App() {
   );
 }
 
-const defaultScore = {
-  eventName: 'My Event',
-  teamOne: {
-    name: 'Team 1',
-    score: '0'
-  },
-  teamTwo: {
-    name: 'Team 2',
-    score: '0'
-  }
-};
-
 function ScoreCard() {
   const pubnub = usePubNub();
-  const [channels] = useState<string[]>(['awesome-channel']);
-  const [eventName, setEventName] = useState<string>(defaultScore.eventName)
-  const [teamOneName, setTeamOneName] = useState<string>(defaultScore.teamOne.name)
-  const [teamOneScore, setTeamOneScore] = useState<string>(defaultScore.teamOne.score)
-  const [teamTwoName, setTeamTwoName] = useState<string>(defaultScore.teamTwo.name)
-  const [teamTwoScore, setTeamTwoScore] = useState<string>(defaultScore.teamTwo.score)
+  const [channels] = useState<string[]>(['score-channel']);
+  const [eventName, setEventName] = useState<string>('My Event')
+  const [maxScore, setMaxScore] = useState<string>('10')
+  const [teamOneName, setTeamOneName] = useState<string>('Team 1')
+  const [teamOneScore, setTeamOneScore] = useState<string>('0')
+  const [teamTwoName, setTeamTwoName] = useState<string>('Team 2')
+  const [teamTwoScore, setTeamTwoScore] = useState<string>('0')
   
-  const updateScore = (message: string) => {
+  useEffect(() => {
+    sendScore('send');
+  },[
+      eventName,
+      maxScore,
+      teamOneName,
+      teamOneScore,
+      teamTwoName,
+      teamTwoScore
+    ]);
+
+  const incrementTeamTwoScore = () => {
+    let score = +teamTwoScore + 1;
+    if (score > +maxScore)
+    {
+      score = +maxScore;
+    }
+    setTeamTwoScore((score).toString());
+  };
+
+  const decrementTeamTwoScore = () => {
+    let score = +teamTwoScore - 1;
+    if (score < 0) {
+      score = 0;
+    }
+    setTeamTwoScore((score).toString());
+  };
+  
+  const incrementTeamOneScore = () => {
+    let score = +teamOneScore + 1;
+    if (score > +maxScore)
+    {
+      score = +maxScore;
+    }
+    setTeamOneScore((score).toString());
+  };
+
+  const decrementTeamOneScore = () => {
+    let score = +teamOneScore - 1;
+    if (score < 0) {
+      score = 0;
+    }
+    setTeamOneScore((score).toString());
+  };
+
+  const sendScore = (message: string) => {
     if (message) {
       const score = {
         eventName: eventName,
+        maxScore: maxScore,
         teamOne: {
           name: teamOneName,
           score: teamOneScore
@@ -64,30 +98,71 @@ function ScoreCard() {
       message = JSON.stringify(score);
       pubnub
         .publish({ channel: channels[0], message })
-        .then(() => updateScore(''));
+        .then(() => sendScore(''));
     }
   };
-
   return (
     <Card>
       <CardHeader>
-        <Heading>Sports Cast Score Card</Heading>
+        <Heading color='black'>Sports Cast Score Card</Heading>
       </CardHeader>
       <CardBody>
       <VStack>
-      <Input onChange={e => setEventName(e.target.value)} placeholder='Enter Event Name'/>
-      <HStack>
-        <Input onChange={e => setTeamOneName(e.target.value)} placeholder='Team Name'/>
-        <Input onChange={e => setTeamOneScore(e.target.value)} placeholder='Team Score'/>
-      </HStack>
-      <HStack>
-        <Input onChange={e => setTeamTwoName(e.target.value)} placeholder='Team Name'/>
-        <Input onChange={e => setTeamTwoScore(e.target.value)} placeholder='Team Score'/>
-      </HStack>
+      <InputGroup>
+        <InputLeftAddon>
+          Event Name
+        </InputLeftAddon>
+          <Input 
+            value = {eventName}
+            onChange={e => setEventName(e.target.value)}
+          />
+      </InputGroup>
+      <InputGroup>
+        <InputLeftAddon>
+          Max Score
+        </InputLeftAddon>
+        <Input 
+          value = {maxScore}
+          onChange={e => setMaxScore(e.target.value)}
+        />
+      </InputGroup>
+      <InputGroup>
+        <InputLeftAddon>
+          Team Name
+        </InputLeftAddon>
+        <Input 
+          value = {teamOneName}
+          onChange={e => setTeamOneName(e.target.value)}
+        />
+      </InputGroup>
+        <HStack>
+          <Button onClick={e => decrementTeamOneScore()}>-</Button>
+          <Input 
+            value = {teamOneScore}
+            onChange={e => setTeamOneScore(e.target.value)}
+          />
+          <Button onClick={e => incrementTeamOneScore()}>+</Button>
+        </HStack>
+        <InputGroup>
+          <InputLeftAddon>
+            Team Name
+          </InputLeftAddon>
+          <Input 
+            value = {teamTwoName}
+            onChange={e => setTeamTwoName(e.target.value)} 
+          />
+        </InputGroup>
+        <HStack>
+          <Button onClick={e => decrementTeamTwoScore()}>-</Button>
+          <Input 
+            value = {teamTwoScore}
+            onChange={e => setTeamTwoScore(e.target.value)}
+          />
+          <Button onClick={e => incrementTeamTwoScore()}>+</Button>
+        </HStack>
       </VStack>
       </CardBody>
       <CardFooter>
-        <Button onClick={e => updateScore('send')}>Submit</Button>
       </CardFooter>
     </Card>
   );
--- apps/viewer/src/app.tsx
+++ apps/viewer/src/app.tsx
@@ -1,16 +1,36 @@
-import { Box, Flex, Text, VStack } from '@chakra-ui/react';
-import React, { useEffect } from 'react';
+import { Box, Flex, Text, Heading, VStack, HStack } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import ConfettiExplosion, {ConfettiProps} from 'react-confetti-explosion';
 
 import InfoBar from '@millicast-react/info-bar';
 import InfoLabel from '@millicast-react/info-label';
 import useNotification from '@millicast-react/use-notification';
 import usePageClosePrompt from '@millicast-react/use-page-close-prompt';
 import useViewer from '@millicast-react/use-viewer';
+import Timer from '@millicast-react/timer';
+import ParticipantCount from '@millicast-react/participant-count';
 
 import ViewerVideoTiles from './components/viewer-video-tiles';
 import { NoStream } from './components/no-stream';
 
+import PubNub from 'pubnub';
+import { PubNubProvider, usePubNub } from 'pubnub-react';
+
 import './styles/app.css';
+
+const pubnub = new PubNub({
+  publishKey: 'pub-c-b14caa5c-c35d-4da7-bed8-3fd3564f11bb',
+  subscribeKey: 'sub-c-59bf0b71-9aa4-4168-9e57-139036b8bfd3',
+  uuid: 'lijojatis'
+});
+
+const largeProps: ConfettiProps = {
+  force: 0.8,
+  duration: 3000,
+  particleCount: 300,
+  width: 1600,
+  colors: ['#041E43', '#1471BF', '#5BB4DC', '#FC027B', '#66D805'],
+};
 
 const App = () => {
   const href = new URL(window.location.href);
@@ -47,7 +67,9 @@ const App = () => {
   return (
     <VStack background="background" height="100%" padding="24px" spacing="24px" width="100%">
       <VStack spacing="16px" width="100%">
-        <InfoBar isActive={isStreaming} numViewers={viewerCount} title="Company name" />
+        <PubNubProvider client={pubnub}>
+          <Scoreboard isActive={isStreaming} numViewers={viewerCount}/>  
+        </PubNubProvider>
         {hasMultiStream ? (
           <Flex justifyContent="flex-end" width="100%">
             <InfoLabel
@@ -63,15 +85,15 @@ const App = () => {
         ) : undefined}
       </VStack>
       {mainMediaStream && mainSourceId ? (
-        <ViewerVideoTiles
-          mainMediaStream={mainMediaStream}
-          mainSourceId={mainSourceId}
-          mainQualityOptions={mainQualityOptions}
-          mainStatistics={mainStatistics}
-          projectToMainStream={projectToMainStream}
-          remoteTrackSources={remoteTrackSources}
-          setSourceQuality={setSourceQuality}
-        />
+          <ViewerVideoTiles
+            mainMediaStream={mainMediaStream}
+            mainSourceId={mainSourceId}
+            mainQualityOptions={mainQualityOptions}
+            mainStatistics={mainStatistics}
+            projectToMainStream={projectToMainStream}
+            remoteTrackSources={remoteTrackSources}
+            setSourceQuality={setSourceQuality}
+          />
       ) : (
         <NoStream />
       )}
@@ -81,5 +103,81 @@ const App = () => {
     </VStack>
   );
 };
+
+interface ScoreboardProps {
+  numViewers: number;
+  isActive?: boolean;
+}
+
+const Scoreboard = ({ numViewers, isActive = false }: ScoreboardProps) =>  {
+  const pubnub = usePubNub();
+  const [channels] = useState<string[]>(['score-channel']);
+  const [eventName, setEventName] = useState<string>('')
+  const [maxScore, setMaxScore] = useState<string>('100')
+  const [teamOneName, setTeamOneName] = useState<string>('')
+  const [teamOneScore, setTeamOneScore] = useState<string>('0')
+  const [teamOneWins, setTeamOneWins] = useState(false);
+  const [teamTwoName, setTeamTwoName] = useState<string>('')
+  const [teamTwoScore, setTeamTwoScore] = useState<string>('0 ')
+  const [teamTwoWins, setTeamTwoWins] = useState(false);
+
+
+  const handleMessage = (event: PubNub.MessageEvent) => {
+    const message = event.message;
+    const info = JSON.parse(message);
+    setEventName(info.eventName);
+    setMaxScore(info.maxScore);
+    setTeamOneName(info.teamOne.name);
+    setTeamOneScore(info.teamOne.score);
+    setTeamTwoName(info.teamTwo.name);
+    setTeamTwoScore(info.teamTwo.score);
+  };
+
+  useEffect(() => {
+    const listenerParams = { message: handleMessage }
+    pubnub.addListener(listenerParams);
+    pubnub.subscribe({ channels });
+    return () => {
+        pubnub.unsubscribe({ channels })
+        pubnub.removeListener(listenerParams)
+    }
+  }, [pubnub, channels]);
+
+  useEffect(() => {
+    setTeamOneWins(+teamOneScore >= +maxScore);
+    setTeamTwoWins(+teamTwoScore >= +maxScore);
+  }, [teamOneScore, teamTwoScore, maxScore]);
+
+  return (
+    <HStack
+      alignItems="center"
+      borderRadius="8px"
+      background="dolbyNeutral.800"
+      justifyContent="space-between"
+      padding="8px 24px"
+      test-id="actionBar"
+      width="100%"
+    >
+        <Heading test-id="headingName" as="h3" fontSize="16px" fontWeight={600} lineHeight={1.5}>
+          {eventName}
+        </Heading>
+
+      <HStack>
+        <Text>{teamOneName}</Text>
+        <Text>{teamOneScore}</Text>
+        {teamOneWins && <ConfettiExplosion {...largeProps} />}
+        <Text>|</Text>
+        <Text>{teamTwoScore}</Text>
+        {teamTwoWins && <ConfettiExplosion {...largeProps} />}
+        <Text>{teamTwoName}</Text>
+      </HStack>
+      <HStack gap="20px">
+        {isActive ? <ParticipantCount count={numViewers} /> : undefined}
+        <Timer isActive={isActive} />
+      </HStack>
+
+    </HStack>
+  );
+}
 
 export default App;
